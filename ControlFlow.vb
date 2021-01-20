abc9ae9df8a8f4bb087489a09ed52337b03d6167
Brayden Peck <brayden.peck2@gmail.com>
2021-01-20 08:33:56 -0700

    Add project files.

---
+++ ControlFlow.vb
@@ -0,0 +1,20 @@
+﻿
+Option Explicit On
+Option Strict On
+Option Compare Binary
+Module ControlFlow
+
+    Sub Main()
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        firstNumber = 5
+        secondNumber = 8
+
+        If firstNumber > secondNumber Then
+            Console.WriteLine("first number is Bigger!!")
+        End If
+        Console.Read()
+    End Sub
+
+End Module
